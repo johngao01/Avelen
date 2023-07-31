81adfa2210a25463506b1f803d022b5802615ac2
johngao01 <1649868243@qq.com>
2023-07-31 18:38:34 +0800

    开始shell脚本，crontab -e配置运行此脚本

---
+++ start.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# shellcheck disable=SC2034
+script_dir="$(dirname "$0")"
+LOCKFILE="${script_dir}/start.lock"
+
+# 检查是否有其他实例正在运行
+if [ -f "$LOCKFILE" ]; then
+  echo "Script is already running. Exiting."
+  exit 1
+fi
+
+# 创建锁文件
+touch "$LOCKFILE"
+
+# 在这里执行你的任务
+cd "${script_dir}" || exit
+python3 scrapy.py
+# 删除锁文件
+rm "$LOCKFILE"
