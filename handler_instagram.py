ad60ac2fffe1c96d5889ea2d16ef9298b12dc996
johngao01 <1649868243@qq.com>
2024-02-24 10:43:31 +0800

    处理下载超时

--- handler_instagram.py
+++ handler_instagram.py
@@ -173,7 +173,7 @@ def download_file(media: Media):
         instagram_logger.info('  ' + save_path + "\t" + convert_bytes_to_human_readable(size))
         return size, save_path
     print(media.url)
-    r = requests.get(media.url, headers=download_header, stream=True)
+    r = requests.get(media.url, headers=download_header, stream=True, timeout=30)
     if r.status_code == 200:
         os.makedirs(os.path.dirname(save_path), exist_ok=True)
         with open(save_path, mode='wb') as f:
